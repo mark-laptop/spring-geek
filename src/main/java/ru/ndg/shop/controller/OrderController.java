f06a1a9c841ee667627ca8ddd51f4bcf192c1402
pro <ndg23@bk.ru>
2020-12-20 14:09:07 +0300

    add project

--- src/main/java/ru/ndg/shop/controller/OrderController.java
+++ src/main/java/ru/ndg/shop/controller/OrderController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.ndg.shop.dto.OrderDto;
 import ru.ndg.shop.service.order.OrderService;
 
 import java.util.Map;
@@ -28,5 +30,23 @@ public class OrderController {
         model.addAttribute("ordersPage", orderService.getAll(page, params));
         model.addAttribute("filters_out", params.get("filters_out"));
         return "orders";
+    }
+
+    @GetMapping(value = "{id}")
+    public String showUpdateOrderPage(Model model, @PathVariable(name = "id") Long id) {
+        model.addAttribute("order", orderService.getById(id));
+        return "order_update";
+    }
+
+    @GetMapping(value = "/new")
+    public String showCreateOrderPage(Model model) {
+        model.addAttribute(new OrderDto());
+        return "order_new";
+    }
+
+    @GetMapping(value = "/delete/{id}")
+    public String deleteOrder(@PathVariable(name = "id") Long id) {
+        orderService.delete(id);
+        return "redirect:/orders";
     }
 }
