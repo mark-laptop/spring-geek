942936d36f8442de13a7f0ac50fc9a16220e094a
pro <ndg23@bk.ru>
2020-12-20 14:12:52 +0300

    add project

--- src/main/java/ru/ndg/shop/config/SecurityConfig.java
+++ src/main/java/ru/ndg/shop/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/customer/**").authenticated()
+                .antMatchers("/orders/**").authenticated()
 //                .antMatchers("/**").authenticated()
                 .and()
                 .formLogin()
